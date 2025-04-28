a30747d0368e21b9b7fa70e8b987173f34655e51
Nguyen Viet Manh <gtechsltn@gmail.com>
2025-04-28 14:42:05 +0800

    Update tblUsers-InsertData.sql

--- tblUsers-InsertData.sql
+++ tblUsers-InsertData.sql
@@ -36,37 +36,39 @@ BEGIN
     )
     VALUES
     (
-        CAST(RAND() * 32767 AS SMALLINT),         -- SmallNumber
-        CAST(RAND() * 255 AS TINYINT),             -- TinyNumber
-        ABS(CAST(CHECKSUM(NEWID()) AS BIGINT)),    -- BigNumber
-        CAST(RAND() * 10000 AS MONEY),             -- MoneyAmount
-        CAST(RAND() * 1000 AS SMALLMONEY),         -- SmallMoneyAmount
-        CAST(RAND() * 100000 AS DECIMAL(18,4)),    -- DecimalValue
-        CAST(RAND() * 100000 AS NUMERIC(18,4)),    -- NumericValue
-        RAND() * 100000,                           -- FloatValue
-        RAND() * 100000,                           -- RealValue
-        CAST((RAND() * 1) AS BIT),                 -- BitValue
-        'user' + CAST(@i AS VARCHAR(10)),          -- Username
-        REPLICATE('A', 64),                        -- PasswordHash (simple dummy 64 chars)
-        'Full Name ' + CAST(@i AS VARCHAR(10)),    -- FullName
-        RIGHT('0000000000' + CAST(@i AS VARCHAR(10)), 10), -- NationalId
-        'Bio for user ' + CAST(@i AS VARCHAR(10)), -- Bio
-        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 10000), GETDATE()), -- DateOfBirth
-        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 1000), GETDATE()),  -- RegistrationDate
-        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 30), GETDATE()),    -- LastLogin
-        CAST(DATEADD(SECOND, @i % 86400, '00:00:00') AS TIME),       -- LoginTime
-        SYSDATETIMEOFFSET(),                                         -- TimestampColumn
-        GETDATE(),                                                   -- CreatedAt
+        CAST(RAND() * 32767 AS SMALLINT),                                                         -- SmallNumber
+        CAST(RAND() * 255 AS TINYINT),                                                            -- TinyNumber
+        ABS(CAST(CHECKSUM(NEWID()) AS BIGINT)),                                                   -- BigNumber
+        CAST(RAND() * 10000 AS MONEY),                                                            -- MoneyAmount
+        CAST(RAND() * 1000 AS SMALLMONEY),                                                        -- SmallMoneyAmount
+        CAST(RAND() * 100000 AS DECIMAL(18,4)),                                                   -- DecimalValue
+        CAST(RAND() * 100000 AS NUMERIC(18,4)),                                                   -- NumericValue
+        RAND() * 100000,                                                                          -- FloatValue
+        RAND() * 100000,                                                                          -- RealValue
+
+        CAST((RAND() * 1) AS BIT),                                                                -- BitValue
+
+        'user' + CAST(@i AS VARCHAR(10)),                                                         -- Username
+        REPLICATE('A', 64),                                                                       -- PasswordHash (simple dummy 64 chars)
+        'Full Name ' + CAST(@i AS VARCHAR(10)),                                                   -- FullName
+        RIGHT('0000000000' + CAST(@i AS VARCHAR(10)), 10),                                        -- NationalId
+        'Bio for user ' + CAST(@i AS VARCHAR(10)),                                                -- Bio
+        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 10000), GETDATE()),                              -- DateOfBirth
+        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 1000), GETDATE()),                               -- RegistrationDate
+        DATEADD(DAY, - (ABS(CHECKSUM(NEWID())) % 30), GETDATE()),                                 -- LastLogin
+        CAST(DATEADD(SECOND, @i % 86400, '00:00:00') AS TIME),                                    -- LoginTime
+        SYSDATETIMEOFFSET(),                                                                      -- TimestampColumn
+        GETDATE(),                                                                                -- CreatedAt
         
         -- BINARY FIELDS (DUMMY DATA)
-        CAST(CONVERT(VARBINARY(MAX), 'Profile_' + CAST(@i AS VARCHAR(10))) AS VARBINARY(MAX)), -- ProfilePicture
-        CAST(REPLICATE(CAST(@i AS BINARY(1)), 50) AS BINARY(50)),     -- Document (50 bytes)
-        CAST(CONVERT(VARBINARY(MAX), 'Image_' + CAST(@i AS VARCHAR(10))) AS IMAGE),             -- DeprecatedImage
+        CAST(CONVERT(VARBINARY(MAX), 'Profile_' + CAST(@i AS VARCHAR(10))) AS VARBINARY(MAX)),    -- ProfilePicture
+        CAST(REPLICATE(CAST(@i AS BINARY(1)), 50) AS BINARY(50)),                                 -- Document (50 bytes)
+        CAST(CONVERT(VARBINARY(MAX), 'Image_' + CAST(@i AS VARCHAR(10))) AS IMAGE),               -- DeprecatedImage
         
         -- Other fields
-        NEWID(),                                                     -- UniqueIdentifier
-        '<User><Id>' + CAST(@i AS VARCHAR(10)) + '</Id></User>',     -- XmlData
-        '{"userId":' + CAST(@i AS VARCHAR(10)) + '}'                 -- JsonData
+        NEWID(),                                                                                  -- UniqueIdentifier
+        '<User><Id>' + CAST(@i AS VARCHAR(10)) + '</Id></User>',                                  -- XmlData
+        '{"userId":' + CAST(@i AS VARCHAR(10)) + '}'                                              -- JsonData
     );
 
     SET @i = @i + 1;
